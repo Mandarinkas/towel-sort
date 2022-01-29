5bcae482ee63bff1693fe3ce6aed95fc5a538a64
EvgeniyReut <mevmrefman@gmail.com>
2022-01-29 22:50:39 +0300

    fix: 1 mistake

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@
 module.exports = function towelSort (matrix) {
   if(matrix!=undefined){
     return(
-      matrix.reduce((acc, cur, i) => {
-        cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => acc.push(e));
-        return acc;
+      matrix.reduce(function(temp, cur, i) {
+        cur.sort((a, b) => !(i & 1) ? a - b : b - a).map(e => temp.push(e));
+        return temp;
       }, [])
     );
   }
   else
-  return [];
+    return [];
 }
